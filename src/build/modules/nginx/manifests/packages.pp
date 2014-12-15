b21b8a227173cb4b9b636e2e24f2671bff1b6614
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-15 22:29:28 +0200

    Update

--- src/build/modules/nginx/manifests/packages.pp
+++ src/build/modules/nginx/manifests/packages.pp
@@ -4,7 +4,8 @@ class nginx::packages {
   file { '/etc/apt/sources.list.d/nginx.list':
     ensure => present,
     source => 'puppet:///modules/nginx/etc/apt/sources.list.d/nginx.list',
-    mode => 644
+    mode => 644,
+    require => Exec['/bin/bash -c "wget -q -O - http://nginx.org/keys/nginx_signing.key | apt-key add -"']
   }
 
   exec { 'apt-get update':
