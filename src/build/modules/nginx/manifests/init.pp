d4a51fe4c599e1f39e08cd489e426e3e99d169c6
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-16 16:07:37 +0200

    Update

--- src/build/modules/nginx/manifests/init.pp
+++ src/build/modules/nginx/manifests/init.pp
@@ -2,10 +2,6 @@ class nginx {
   require nginx::packages
   require nginx::supervisor
 
-  exec { 'mkdir -p /nginx/{data,ssl/{certs,private}}':
-    path => ['/bin']
-  }
-
   file { '/etc/nginx/conf.d/example_ssl.conf':
     ensure => absent
   }
